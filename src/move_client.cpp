b7a1147f4aca18680f754c2abeff8ab0d58cafd6
vm_introrob <angelahu2001@gmail.com>
2023-02-13 07:48:05 -0800

    client compila

--- src/move_client.cpp
+++ src/move_client.cpp
@@ -1,17 +1,15 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <ur5lego/MoveAction.h>
-
-using namespace actionlib;
+#include <actionlib_tutorials/FibonacciAction.h>
 
 int main (int argc, char **argv)
 {
-  ros::init(argc, argv, "test_move");
+  ros::init(argc, argv, "test_fibonacci");
 
   // create the action client
   // true causes the client to spin its own thread
-  actionlib::SimpleActionClient<ur5lego::MoveAction> ac("Move", true);
+  actionlib::SimpleActionClient<actionlib_tutorials::FibonacciAction> ac("fibonacci", true);
 
   ROS_INFO("Waiting for action server to start.");
   // wait for the action server to start
