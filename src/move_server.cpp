3a03e473e48cc42d8075cf59b778be9a82389655
Fre <francescopiazzi25@gmail.com>
2023-02-13 16:17:41 +0100

    server azione compila

--- src/move_server.cpp
+++ src/move_server.cpp
@@ -1,83 +1,40 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
-#include <actionlib_tutorials/FibonacciAction.h>
+#include <ur5lego/MoveAction.h>
 
-class FibonacciAction
+class MoveAction
 {
 protected:
-
-  ros::NodeHandle nh_;
-  actionlib::SimpleActionServer<actionlib_tutorials::FibonacciAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
-  std::string action_name_;
-  // create messages that are used to published feedback/result
-  actionlib_tutorials::FibonacciFeedback feedback_;
-  actionlib_tutorials::FibonacciResult result_;
+    ros::NodeHandle nh_;
+    // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+    actionlib::SimpleActionServer<ur5lego::MoveAction> action_server_;
+    std::string action_name_;
+    // create messages that are used to published feedback/result
+    ur5lego::MoveFeedback feedback_;
+    ur5lego::MoveResult result_;
 
 public:
-
-  FibonacciAction(std::string name) :
-    as_(nh_, name, boost::bind(&FibonacciAction::executeCB, this, _1), false),
-    action_name_(name)
-  {
-    as_.start();
-  }
-
-  ~FibonacciAction(void)
-  {
-  }
-
-  void executeCB(const actionlib_tutorials::FibonacciGoalConstPtr &goal)
-  {
-    // helper variables
-    ros::Rate r(1);
-    bool success = true;
-
-    // push_back the seeds for the fibonacci sequence
-    feedback_.sequence.clear();
-    feedback_.sequence.push_back(0);
-    feedback_.sequence.push_back(1);
-
-    // publish info to the console for the user
-    ROS_INFO("%s: Executing, creating fibonacci sequence of order %i with seeds %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
-
-    // start executing the action
-    for(int i=1; i<=goal->order; i++)
+    MoveAction(std::string name) : action_server_(nh_, name, boost::bind(&MoveAction::executeCB, this, _1), false), action_name_(name)
     {
-      // check that preempt has not been requested by the client
-      if (as_.isPreemptRequested() || !ros::ok())
-      {
-        ROS_INFO("%s: Preempted", action_name_.c_str());
-        // set the action state to preempted
-        as_.setPreempted();
-        success = false;
-        break;
-      }
-      feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i-1]);
-      // publish the feedback
-      as_.publishFeedback(feedback_);
-      // this sleep is not necessary, the sequence is computed at 1 Hz for demonstration purposes
-      r.sleep();
+        action_server_.start();
     }
 
-    if(success)
+    ~MoveAction(void)
     {
-      result_.sequence = feedback_.sequence;
-      ROS_INFO("%s: Succeeded", action_name_.c_str());
-      // set the action state to succeeded
-      as_.setSucceeded(result_);
     }
-  }
 
-
+    void executeCB(const ur5lego::MoveGoalConstPtr &goal)
+    {
+        ROS_INFO("Executing... target:(%i,%i,%i), (%i,%i,%i)", goal->X, goal->Y, goal->Z, goal->r, goal->p, goal->y);
+    }
 };
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "fibonacci");
+    ros::init(argc, argv, "fibonacci");
 
-  FibonacciAction fibonacci("fibonacci");
-  ros::spin();
+    MoveAction fibonacci("fibonacci");
+    ros::spin();
 
-  return 0;
+    return 0;
 }
