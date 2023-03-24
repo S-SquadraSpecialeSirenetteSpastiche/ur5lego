55aa849b04baa67b486f6cd796f9801d0929fb02
Fre <francescopiazzi25@gmail.com>
2023-03-24 17:13:08 +0100

    destinations are now read from a temporary file

--- src/move_client.cpp
+++ src/move_client.cpp
@@ -2,40 +2,60 @@
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <ur5lego/MoveAction.h>
+#include <fstream>
 
 int main (int argc, char **argv)
 {
-  ros::init(argc, argv, "action_client");
+    ros::init(argc, argv, "action_client");
 
-  // create the action client
-  // true causes the client to spin its own thread
-  actionlib::SimpleActionClient<ur5lego::MoveAction> ac("move_server", true);
+    // create the action client
+    // true causes the client to spin its own thread
+    actionlib::SimpleActionClient<ur5lego::MoveAction> ac("move_server", true);
 
-  ROS_INFO("Waiting for action server to start.");
-  // wait for the action server to start
-  ac.waitForServer(); //will wait for infinite time
+    ROS_INFO("Waiting for action server to start.");
+    // wait for the action server to start
+    ac.waitForServer(); //will wait for infinite time
 
-  ROS_INFO("Action server started, sending goal.");
-  // send a goal to the action
-  ur5lego::MoveGoal goal;
-  goal.X = (_Float32)0.1;
-  goal.Y = (_Float32)0.0;
-  goal.Z = (_Float32)-0.2;
-  goal.r = (_Float32)0;
-  goal.p = (_Float32)0;
-  goal.y = (_Float32)0;
-  ac.sendGoal(goal);
+    ROS_INFO("Action server started, sending goal.");
+    // send a goal to the action
+    ur5lego::MoveGoal goal;
 
-  //wait for the action to return
-  bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+    std::ifstream file("/home/utente/my_ws/src/ur5lego/src/dest.txt");
 
-  if (finished_before_timeout)
-  {
-    ROS_INFO("Action finished");
-  }
-  else
-    ROS_INFO("Action did not finish before the time out.");
+    // while(!eof(file)) o qualcosa del genere
 
-  //exit
-  return 0;
+    // su Gazebo asse rosso x, verde y, blu z
+    float X, Y, Z, r, p, y;
+    file >> X >> Y >> X >> r >> p >> y;
+    ROS_INFO("Action server started, sending goal.");
+    goal.X = (_Float32)X;
+    goal.Y = (_Float32)Y;
+    goal.Z = (_Float32)Z;
+    goal.r = (_Float32)r;
+    goal.p = (_Float32)p;
+    goal.y = (_Float32)y;
+    ROS_INFO_STREAM(X);
+    ROS_INFO_STREAM(goal.X);
+    file.close();
+
+    /*
+    goal.X = (_Float32)0.1;
+    goal.Y = (_Float32)0.0;
+    goal.Z = (_Float32)-0.2;
+    goal.r = (_Float32)0;
+    goal.p = (_Float32)0;
+    goal.y = (_Float32)0;
+    */
+
+    ac.sendGoal(goal);
+
+    //wait for the action to return
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(20.0));
+    if (finished_before_timeout)
+        ROS_INFO("Action finished");
+    else
+        ROS_INFO("Action did not finish before the time out.");
+
+
+    return 0;
 }
