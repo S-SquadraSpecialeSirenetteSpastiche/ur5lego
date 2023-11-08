87fdd71814937ca2577eb16213f30120811cdac3
Fre <francescopiazzi25@gmail.com>
2023-11-08 15:31:19 +0100

    cleanup, reduced iteration to fail and steps

--- src/inverse_kinematics.cpp
+++ src/inverse_kinematics.cpp
@@ -1,6 +1,4 @@
 #include "include/inverse_kinematics.h"
-#include <chrono>
-#include <ros/ros.h>
 
 
 /// @brief computes the inverse kinematics of a 6DOF robot
@@ -11,14 +9,16 @@
 /// @return a pair containing the solution of the inverse kinematics and a boolean value indicating if the algorithm was successful
 std::pair<Eigen::VectorXd, bool> inverse_kinematics_wrapper(
     pinocchio::Model model, Eigen::Vector3d target_position, Eigen::Vector3d target_orientation_rpy, Eigen::VectorXd q0){
-    
+
     pinocchio::Data data(model);
     pinocchio::FrameIndex frame_id = model.getFrameId("ee_link", (pinocchio::FrameType)pinocchio::BODY);
 
     pinocchio::computeAllTerms(model, data, q0, Eigen::VectorXd::Zero(model.nv));
     pinocchio::SE3 start_pos = pinocchio::updateFramePlacement(model, data, frame_id);
 
-    int n_steps = 10;
+    // float distance = (target_position - start_pos.translation(), errorInSO3(start_pos.rotation(), euler_to_rotation_matrix(Eigen::Vector3d(target_orientation_rpy)))).norm();
+    // ROS_INFO_STREAM("distance: " << distance);
+    const int n_steps = 4;
 
     Eigen::VectorXd q(6);
     Eigen::Vector3d position_sofar = start_pos.translation();
@@ -31,7 +31,6 @@ std::pair<Eigen::VectorXd, bool> inverse_kinematics_wrapper(
         step_size[i+3] = (target_orientation_rpy[i] - orientation_sofar[i])/(double)n_steps;
     }
 
-    // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     for(int i=0; i<n_steps; i++){
         for(int j=0; j<3; j++){
             position_sofar[j] += step_size[j];
@@ -43,11 +42,10 @@ std::pair<Eigen::VectorXd, bool> inverse_kinematics_wrapper(
             return std::make_pair(q, false);    // il valore di q qui è irrilevante perchè l'algoritmo non ha avuto successo
         q = ikresult.first;
     }
-    // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    // ROS_INFO_STREAM("Time for ik algorithm = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "[ms]");
 
     return std::make_pair(q, true);
 }
+
 
 /// @brief computes the inverse kinematics of a 6DOF robot, this function only works for small movements and therefore should not be called directly
 /// @param model                the robot model
@@ -64,15 +62,14 @@ std::pair<Eigen::VectorXd, bool> inverse_kinematics(
     pinocchio::Data data(model);
 
     double eps = 1e-6;
-    double alpha = 1, beta = 0.5, gamma = 0.5;
+    double alpha = 1, beta = 0.5;
 
     int niter = 0;  // iterations so far
-    int max_iter = 200;
+    int max_iter = 20;
 
     double lambda = 1e-8;
     bool out_of_workspace = false;
     bool success = false;
-
 
     while(niter < max_iter){ 
         // compute position and orientation of the ee with the current guess
@@ -102,7 +99,7 @@ std::pair<Eigen::VectorXd, bool> inverse_kinematics(
         Eigen::VectorXd dq(model.nv);
         dq = (jacobian + lambda*Eigen::MatrixXd::Identity(6, model.nv)).inverse()*e_bar_q0;
         Eigen::VectorXd q1(model.nv);
-
+        
         while(true){
             // compute new guess and the position of the ee with the new guess
             q1 = q0 + dq * alpha;
