5bfb3d4d1c13fcf67ebc0fe69ec9ab840ee4b0e7
CottonCandy01 <angelahu2001@gmail.com>
2023-12-07 00:08:58 +0100

    Errore scononsciuto, non compila

--- src/include/trajectory_planner.h
+++ src/include/trajectory_planner.h
@@ -1,8 +1,13 @@
+#ifndef TRAJECTORY_PLANNER_H
+#define TRAJECTORY_PLANNER_H
 #include "Eigen/Eigen"
 #include "math_tools.h"
 #include <ros/ros.h>
 
 
-void send_joint_positions(ros::Publisher publisher, Eigen::VectorXd q);
+void send_arm_joint_angles(Eigen::VectorXd q, ros::Publisher publisher);
+void send_gripper_joint_angles(_Float64 q, ros::Publisher publisher);
 
 void computeAndSendTrajectory(Eigen::VectorXd q, Eigen::VectorXd qf, float t, int steps, ros::Publisher publisher);
+void computeAndSendTrajectory(_Float64 qi, _Float64 qf, float tf, int steps, ros::Publisher publisher)
+#endif
--- src/movement/move_server.cpp
+++ src/movement/move_server.cpp
@@ -8,7 +8,6 @@
 #include <ros/package.h>
 #include <actionlib/server/simple_action_server.h>
 #include <ur5lego/MoveAction.h>
-#include <ur5lego/GripperAction.h>
 #include <string>
 
 
@@ -47,10 +46,7 @@ public:
         const std::string urdf_file = ros::package::getPath(PACKAGE_NAME) + std::string(UR_DESCRIPTION);
         pinocchio::urdf::buildModel(urdf_file, model_);
         q = Eigen::VectorXd(6);
-        q_curr = Eigen::VectorXd(9);
-        q_gripper = Eigen::VectorXd::Zero(3);
         q << -0.32, -0.78, -2.56, -1.63, -1.57, 3.49;   // homing position
-        q_curr << q, q_gripper;
 
         action_server_.start();
     }
@@ -68,19 +64,8 @@ public:
         std::pair<Eigen::VectorXd, bool> res = inverse_kinematics(
             model_, Eigen::Vector3d(goal->X, goal->Y, goal->Z), Eigen::Vector3d(goal->r, goal->p, goal->y), q);
         
-        Eigen::VectorXd q_def = Eigen::VectorXd(9);
         if(res.second){
-            if(gripper_open){
-                q_gripper[0] = 1.0;
-                q_gripper[1] = 1.0;
-                q_gripper[2] = 1.0;
-            }else{
-                q_gripper[0] = 0.0;
-                q_gripper[1] = 0.0;
-                q_gripper[2] = 0.0;
-            }
-            q_def << res.first, q_gripper;
-            computeAndSendTrajectory(q_curr, q_def, goal->time, 200, publisher);
+            computeAndSendTrajectory(q, res.first, goal->time, 200, publisher);
             q = res.first;
             ROS_INFO("Inverse kinematics succeded");
         } else {
@@ -101,83 +86,13 @@ private:
     } 
 };
 
-class GripperAction
-{
-protected:
-    const std::string PUBLISHING_CHANNEL = "/ur5/joint_group_pos_controller/command";
-
-    // node that acts as action server
-    ros::NodeHandle server_node;
-    // node that sends commands to the robot
-    ros::NodeHandle talker_node;
-    ros::Publisher publisher;
-    // NodeHandle instance must be created before this line
-    actionlib::SimpleActionServer<ur5lego::GripperAction> action_server_;
-    std::string action_name_;
-    // create messages that are used to published feedback/result
-    ur5lego::GripperFeedback feedback_;
-    ur5lego::GripperResult result_;
-    
-    pinocchio::Model model_;    // the model of the robot
-
-public:
-    GripperAction(std::string name) : action_server_(server_node, name, boost::bind(&GripperAction::executeCB, this, _1), false), action_name_(name)
-    {
-        publisher = talker_node.advertise<std_msgs::Float64MultiArray>(PUBLISHING_CHANNEL, 10);
-        // q = Eigen::VectorXd(9);
-
-        action_server_.start();
-    }
-
-    ~GripperAction(void)
-    {
-    }
-    //TODO: not clear how to implememtn it, for now it's only a copy of first server 
-    /// @brief callback for the action server
-    /// @param goal the goal sent by the client
-    void executeCB(const ur5lego::GripperGoalConstPtr &goal){
-        ROS_INFO_STREAM("Received goal: " << coordsToStr(goal->finger) << " to do in " << goal->time << "s");
-
-        Eigen::VectorXd q_temp = Eigen::VectorXd(9);
-        q_temp << q;
-        q_temp << goal->finger;
-        q_temp << goal->finger;
-        q_temp << goal->finger;
-        if(goal->finger != 0.0){
-            gripper_open = false;
-        }
-        else{
-            gripper_open = true;
-        }
-            
-        computeAndSendTrajectory(q_curr , q_temp, goal->time, 200, publisher);
-        for(int i = 6; i < 9; i ++){
-            q_curr[i] = q_temp[i];
-        }
-
-        action_server_.setSucceeded(result_);
-    }    
-
-private: 
-    /// @brief converts the coordinates to a string
-    std::string coordsToStr(float X){
-        std::stringstream ss;
-        ss << std::fixed << std::setprecision(2);
-        ss << "(" <<  X << ")";
-        return ss.str();
-    }
-};
-
-
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "action_server");
+    ros::init(argc, argv, "move_server");
 
     MoveAction moveAction("move_server");
-    // GripperAction gripperAction("gripper_server");
 
     ROS_INFO_STREAM("Move server ready.");
-    ROS_INFO_STREAM("Gripper server ready.");
 
     ros::spin();
 
