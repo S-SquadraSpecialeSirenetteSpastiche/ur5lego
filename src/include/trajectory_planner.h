f44a70b119466c2c5819a76ebbe0b025b869f4b8
CottonCandy01 <angelahu2001@gmail.com>
2023-12-07 00:26:26 +0100

    COMPILA mannaggia al ;

--- src/include/trajectory_planner.h
+++ src/include/trajectory_planner.h
@@ -3,11 +3,13 @@
 #include "Eigen/Eigen"
 #include "math_tools.h"
 #include <ros/ros.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
 
 
 void send_arm_joint_angles(Eigen::VectorXd q, ros::Publisher publisher);
 void send_gripper_joint_angles(_Float64 q, ros::Publisher publisher);
 
 void computeAndSendTrajectory(Eigen::VectorXd q, Eigen::VectorXd qf, float t, int steps, ros::Publisher publisher);
-void computeAndSendTrajectory(_Float64 qi, _Float64 qf, float tf, int steps, ros::Publisher publisher)
+void computeAndSendTrajectory(_Float64 qi, _Float64 qf, float tf, int steps, ros::Publisher publisher);
 #endif
