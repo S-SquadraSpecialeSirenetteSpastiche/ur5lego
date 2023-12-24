5205ab1368967454fd7283768072e43592127b88
Fre <francescopiazzi25@gmail.com>
2023-12-24 22:09:48 +0100

    added cache to improve efficiency of the ik algorithm

---
+++ src/include/cache_handler.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+#include <Eigen/Core>
+
+struct Point3D {
+    double x;
+    double y;
+    double z;
+};
+
+typedef std::vector<std::pair<Point3D, Eigen::VectorXd>> Cache;
+
+Cache parse_cache(const std::string& filepath);
+
+Eigen::VectorXd find_closest(const Cache& data, double X, double Y, double Z);
